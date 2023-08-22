7a5a00052b209d08a730e8c615accd1231fcf6c5
aqsa khan <aqsa.khan@novusaltair.com>
2023-08-22 14:53:46 +0500

    22 aug

---
+++ lib/MVVM/data/network/NetworkApiSerice.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:firebaseflutterproject/MVVM/data/app_exceptions.dart';
+import 'package:firebaseflutterproject/MVVM/data/network/baseApiServices.dart';
+import 'package:http/http.dart' as http ;
+
+class NetworkApiService extends BaseApiServices{
+  @override
+  Future getGetAppResponse(String url) async{
+    // TODO: implement getGetAppResponse
+    dynamic responseJson;
+   try{
+
+     final response=await http.get(Uri.parse(url));
+     responseJson=returnReponse(response);
+
+   }
+       on SocketException{
+     return throw FetchDataException('No Internet Connection');
+       }
+   return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url,dynamic data) async {
+    // TODO: implement getPostApiResponse
+    dynamic responseJson;
+    try{
+
+      http.Response response=await http.post(
+          Uri.parse(url),
+
+      body: data);
+      responseJson=returnReponse(response);
+
+    }
+    on SocketException{
+      return throw FetchDataException('No Internet Connection');
+    }
+    return responseJson;
+  }
+      dynamic returnReponse(http.Response response) {
+
+    switch(response.statusCode){
+      case 200:
+       dynamic responseJson=jsonDecode(response.body);
+       return responseJson;
+      case 400:
+    throw BadRequestException("${response.body}");
+
+      default:
+        throw FetchDataException("Error occured  while communicating with server"+"satatus ${response.statusCode.toString()}");
+
+
+    }
+
+      }
+
+
+}
---
+++ lib/MVVM/data/network/baseApiServices.dart
@@ -0,0 +1,5 @@
+
+abstract class BaseApiServices{
+  Future<dynamic> getGetAppResponse(String url);
+  Future<dynamic> getPostApiResponse(String url,dynamic data);
+}
