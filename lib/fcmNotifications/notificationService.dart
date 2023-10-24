17d64d912d1694a9c409e8b6dc162040f24ffb4b
aqsakhan22 <toheed.wajid@bmacapital.com>
2023-10-24 16:25:45 +0500

    24 fcm notification with image

--- lib/fcmNotifications/notificationService.dart
+++ lib/fcmNotifications/notificationService.dart
@@ -5,6 +5,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
 
 // Fcm Notification service with image
+//https://stackoverflow.com/questions/71053724/flutter-local-notifications-plugin-not-showing-the-big-image-in-notification
 
 class FcmNotificationService {
   final FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
